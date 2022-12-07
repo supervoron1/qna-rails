9c2be7d2747ade53d3867009847bd5543d6f5263
raven <vorontsof@mail.ru>
2022-12-07 19:50:03 +0400

    lesson9  Feature: delete answer link spec

--- spec/features/answers/delete_link_spec.rb
+++ spec/features/answers/delete_link_spec.rb
@@ -1,31 +1,34 @@
 require 'rails_helper'
 
-feature 'User can delete link from question', %q{
-  In order to delete wrong link from question
+feature 'User can delete link from answer', %q{
+  In order to delete wrong link from answer
   As an authenticated user
   I'd like to be able to delete link
 } do
 
   given(:user) { create(:user) }
   given!(:question) { create(:question, user: user) }
-  given(:not_own_question) { create(:question)}
+  given!(:answer) { create(:answer, question: question, user: user) }
   given!(:link) { create(:link, linkable: question) }
-  given(:another_link) {create(:link, linkable: not_own_question) }
+
+  given!(:another_question) { create(:question) }
+  given!(:another_answer) { create(:answer, question: another_question) }
+  given(:another_link) {create(:link, linkable: another_answer) }
 
   describe 'Authenticated user', js: true do
     background do
       sign_in(user)
     end
 
-    scenario 'deletes link from his question' do
+    scenario 'deletes link from his answer' do
       visit question_path(question)
       click_on 'Delete link'
 
       expect(page).to_not have_link link.name
     end
 
-    scenario "deletes link from not his question" do
-      visit question_path(not_own_question)
+    scenario "deletes link from not his answer" do
+      visit question_path(another_question)
 
       expect(page).to_not have_link('Delete link')
     end
