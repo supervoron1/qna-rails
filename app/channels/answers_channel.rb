10c3abf199f27107210d096e241029dfeb0458f3
raven <vorontsof@mail.ru>
2022-12-23 13:51:48 +0400

    lesson11 hotfix

--- app/channels/answers_channel.rb
+++ app/channels/answers_channel.rb
@@ -1,7 +1,6 @@
 class AnswersChannel < ApplicationCable::Channel
   def follow
-    # stream_from "answers"
+    stream_from "answers"
     # stream_from "answers/#{params[:question_id]}"
-    stream_from "answers_channel_#{ params[:question_id] }"
   end
 end
